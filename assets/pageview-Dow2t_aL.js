6dd9b7a1b7045a870771fa95fd3ee1b539c78a36
lixuanfengs <1183895890@qq.com>
2024-05-30 09:50:16 +0000

    Deploying to gh-pages from @ lixuanfengs/Cactus@e97eb6a89683d21f45fed3fa6a0a71e17bdd8d1b 🚀

--- assets/pageview-Dow2t_aL.js
+++
@@ -1,1 +0,0 @@
-import{i as r}from"./app-BkkTPHds.js";const a=async()=>{try{const{pageviewCount:e}=await r(()=>import("./app-BkkTPHds.js").then(t=>t.Y),[]);return e({serverURL:COMMENT_OPTIONS.serverURL})}catch{console.error("@waline/client is not installed!")}};export{a as updatePageview};
